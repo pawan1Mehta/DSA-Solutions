0215ded664e94904692413abe76659056c99ccda
Pawan Mehta <kpkumar.8757@gmail.com>
2026-06-07 22:13:40 +0530

    Time: 19 ms (0%), Space: 193.2 MB (0%) - LeetHub

---
+++ 3951-minimum-energy-to-maintain-brightness/3951-minimum-energy-to-maintain-brightness.java
@@ -0,0 +1,53 @@
+class Solution {
+    public long minEnergy(int n, int brightness, int[][] intervals_) {
+        int[][] intervals = merge(intervals_);
+
+        long needLightsOn = (brightness % 3 == 0 ? (brightness/3) : (brightness/3) + 1);
+
+        long activeTime = 0;
+
+        for(int i = 0; i < intervals.length; i++) {
+            activeTime += (intervals[i][1] - intervals[i][0] + 1);
+        }
+
+        return needLightsOn * activeTime;
+    }
+
+    public int[][] merge(int[][] intervals) {
+        int n = intervals.length;
+
+        Arrays.sort(intervals, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                if(a[0] == b[0]) {
+                    return Integer.compare(a[1], b[1]);
+                }
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+
+        ArrayList<int[]> res = new ArrayList<>();
+
+        int[] curr = intervals[0];
+        for(int i = 1; i < n; i++) {
+            if(isOverlaping(curr, intervals[i])) {
+                curr[1] = Math.max(curr[1], intervals[i][1]);
+            } else {
+                res.add(curr);
+                curr = intervals[i];
+            }
+        }
+
+        res.add(curr);
+
+        int[][] resArr = new int[res.size()][2];
+        for(int i = 0; i < res.size(); i++) {
+            resArr[i] = res.get(i);
+        }
+
+        return resArr;
+    }
+
+    private boolean isOverlaping(int[] a, int[] b) {
+        return b[0] <= a[1] ? true : false;
+    }
+}
