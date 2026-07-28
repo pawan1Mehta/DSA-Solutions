b6a8332270d3ee8feeee3c45f5fd4f52f7968860
Pawan Mehta <kpkumar.8757@gmail.com>
2026-07-28 10:55:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Shortest Path in 1-2 Graph/shortest-path-in-12-graph.java
+++ Difficulty: Hard/Shortest Path in 1-2 Graph/shortest-path-in-12-graph.java
@@ -1,61 +1,66 @@
 class Solution {
-    public int shortestPath(int V, int src, int dest, int[][] edges) {
-        ArrayList<ArrayList<int[]>> adjList = constructAdjList(V, edges);
+    
+    private int bfs(ArrayList<ArrayList<Integer>> adjList, int src, int dest) {
+        Queue<int[]> queue = new LinkedList<>();
+        Set<Integer> visited = new HashSet<>();
         
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>(new Comparator<int[]>(){
-            public int compare(int[] a, int[] b) {
-                return Integer.compare(a[1], b[1]);
-            }
-        });
+        queue.add(new int[]{src, 0});
+        visited.add(src);
         
-        int[] dist = new int[V];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        
-        minHeap.add(new int[]{src, 0});
-        dist[src] = 0;
-        
-        while(!minHeap.isEmpty()) {
-            int[] curr = minHeap.poll();
-            int u = curr[0];
-            int currDist = curr[1];
-            
-            if(dist[u] < currDist) {
-                continue;
-            }
-            
-            for(int[] adjNode : adjList.get(u)) {
-                int v = adjNode[0];
-                int wt = adjNode[1];
+        while(!queue.isEmpty()) {
+            int n = queue.size();
+            while(n-- > 0) {
+                int[] curr = queue.poll();
                 
-                if(dist[v] > (dist[u] + wt)) {
-                    dist[v] = dist[u] + wt;
-                    minHeap.add(new int[]{v, dist[v]});
+                int node = curr[0];
+                int count = curr[1];
+                
+                if(node == dest) {
+                    return count;
+                }
+                
+                for(int adjNode : adjList.get(node)) {
+                    if(!visited.contains(adjNode)) {
+                        queue.add(new int[]{adjNode, count + 1});
+                        visited.add(adjNode);
+                    }
                 }
             }
         }
         
-        int res = dist[dest];
-        
-        return res == Integer.MAX_VALUE ? -1 : res;
+        return -1;
     }
     
-    private ArrayList<ArrayList<int[]>> constructAdjList(int V, int[][] edges) {
-        ArrayList<ArrayList<int[]>> adjList = new ArrayList<>();
-
-        for (int i = 0; i < V; i++) {
-            adjList.add(new ArrayList<>());
+    public int shortestPath(int V, int src, int dest, int[][] edges) {
+        int extraNode = V;
+        
+        ArrayList<ArrayList<Integer>> adjList = new ArrayList<>();
+        
+        for(int i = 0; i < (V + edges.length); i++) {
+            adjList.add(new ArrayList<>());    
         }
-
+        
         int u, v, wt;
-        for (int[] edge : edges) {
+        for(int[] edge : edges) {
             u = edge[0];
             v = edge[1];
             wt = edge[2];
             
-            adjList.get(u).add(new int[]{v, wt});
-            adjList.get(v).add(new int[]{u, wt});
+            if(wt == 1) {
+                adjList.get(u).add(v);
+                adjList.get(v).add(u);
+            } else {
+                
+                adjList.get(u).add(extraNode);
+                adjList.get(extraNode).add(u);
+                
+                adjList.get(extraNode).add(v);
+                adjList.get(v).add(extraNode);
+                
+                extraNode++;
+            }
         }
-
-        return adjList;
+        
+        return bfs(adjList, src, dest);
     }
 }
